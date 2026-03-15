9b777245f49a59156fc86b8073a785994135ea69
rizeliyucel0-lang <rizeliyucel0@gmail.com>
2026-03-16 00:12:29 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:12:28.413Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-15T21:12:29.444Z
+Commit: Fix broken links in documentation
