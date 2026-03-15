163124d5fb9a786b30fd382bcd6e924bda870390
rizeliyucel0-lang <rizeliyucel0@gmail.com>
2026-03-16 00:13:51 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:13:35.665Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-15T21:13:51.606Z
+Commit: Introduce real-time notifications
