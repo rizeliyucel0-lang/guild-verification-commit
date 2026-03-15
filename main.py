260b1080355d9280707449f3d5688664e0ebaa70
rizeliyucel0-lang <rizeliyucel0@gmail.com>
2026-03-16 00:13:12 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:13:03.251Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-15T21:13:12.358Z
+Commit: Fix broken links in documentation
