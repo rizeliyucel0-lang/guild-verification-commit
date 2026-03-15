fc7a0c47669fcaebd8f1be2af0c0dcea78f9a279
rizeliyucel0-lang <rizeliyucel0@gmail.com>
2026-03-16 00:13:25 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:13:20.255Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-15T21:13:25.613Z
+Commit: Fix CSS styling bug on mobile devices
