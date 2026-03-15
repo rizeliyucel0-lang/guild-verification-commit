63902a8692fcba05902417280c1e25c75d45466d
rizeliyucel0-lang <rizeliyucel0@gmail.com>
2026-03-16 00:12:30 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:12:29.444Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-15T21:12:30.570Z
+Commit: Fix race condition in async operations
