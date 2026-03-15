9c5823bb19a2c6be459fa64c8075e368ef9f7d6b
rizeliyucel0-lang <rizeliyucel0@gmail.com>
2026-03-16 00:13:36 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T21:13:27.734Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-15T21:13:36.784Z
+Commit: Fix CSS styling bug on mobile devices
